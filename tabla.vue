c232ef337b23d8b3a315d6181dbaf6369c665700
BACD2626 <brandoncampos2626@gmail.com>
2018-11-19 17:46:36 -0500

    Update tabla.vue

--- tabla.vue
+++ tabla.vue
@@ -6,21 +6,47 @@
 </template>
 
 <script>
-export default {
-  name:'',
-  data () {
-      todos:[
-          {nombre: 'nombre',apellido:'apellido',ciudad:'ciudad',sexo:'sexo',fecha:'fecha de nacimiento', direc:"direccion de entrega"}
+vue.component('todo-item', {
+props:['todo'],
+template: `<li>{{todo.nombre}}<li>
+`
+})
+
+import Vue from "vue";
+vue.component('todo-list',{
+props: ['todos'],
+template: `
+ <ul>
+    <todo-item
+        v-for="(todo,index) in todos"
+        :todo="todo">
+    </todo-item>
+ </ul>
+`
+
+})
+const app = new Vue({
+    el: 'app',
+    data: {
+        todos:[
+          {nombre: 'nombre',
+          apellido:'apellido',
+          ciudad:'ciudad',
+          sexo:'sexo',
+          fecha:'fecha de nacimiento',
+          direc:'direccion de entrega'}
       ]
+    },
+    template: `
+    <todo-list :todos="todos"></todo-list>
+    `
+})
 
-    
-  }
-  
 
-  
-}
 </script>
 
 <style>
 
 </style>
+
+      ]
