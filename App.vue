1a6ecc6bf535d910faf7d60c9963891df4b0f88f
BACD2626 <brandoncampos2626@gmail.com>
2018-11-19 17:44:41 -0500

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,16 +1,19 @@
 <template>
-  <div id="app" v-bind:style="{ backgroundColor: color}">
+  <div id="app" >
     
      <div >
-       
-      <formulario/>
+     
+      
+      <login/>
       
 
-     </div>
+    </div>
      
     <div >
       
-      <login/>
+      
+      <formulario/>
+
       
      
     </div>
@@ -18,19 +21,16 @@
 </template>
 
 <script>
+
 import login from './components/login.vue'
 import formulario from './components/formulario.vue'
 export default {
   name: 'app',
   components:{
-    login, formulario
+    login, formulario,
   },
   data () {
     
-    todos;[
-          {nombre: 'nombre',apellido:'apellido',ciudad:'ciudad',sexo:'sexo',fecha:'fecha de nacimiento', direc:"direccion de entrega"}
-      ]
-
     
     return {
       
