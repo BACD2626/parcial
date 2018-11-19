1bebdd178daeafba0db75371a32b5fce572622a9
BACD2626 <brandoncampos2626@gmail.com>
2018-11-19 17:46:14 -0500

    Update login.vue

--- login.vue
+++ login.vue
@@ -1,24 +1,30 @@
 <template>
+
+
   <div>
-      <h1>login</h1>
+      <h1>LOGIN</h1>
       
       <input type=“text” placeholder=usuario name=“txtusuario” /><br> <br>
       
       <input type=“text” placeholder=contraseña name=“txtusuario” />   <br> <br>
       <button v-on:click="say('hi')">login</button><br><br> <br>
-      
+      <ul>
+          
+      </ul>
       
 
       
-      <br> <br><br>      
+      <br><br><br>      
       
   </div>
 </template>
 
 <script>
+
 export default {
   name:'',
   data () {
+
       
     return {
       
